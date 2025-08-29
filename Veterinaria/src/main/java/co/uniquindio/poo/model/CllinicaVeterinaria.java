214ae4d8e2e705f374d9b10737f6b296005dfef9
natag30 <149449326+natag30@users.noreply.github.com>
2025-08-28 21:40:41 -0500

    update metodos

--- Veterinaria/src/main/java/co/uniquindio/poo/model/CllinicaVeterinaria.java
+++ Veterinaria/src/main/java/co/uniquindio/poo/model/CllinicaVeterinaria.java
@@ -66,4 +66,80 @@ public class CllinicaVeterinaria {
     public void registrarPropietarioClinica(Propietario propietario){
         listPropietarios.add(propietario);
     }
+
+    //métodos get y set no se agregan en el diagrama por practicidad
+
+    /**
+     * Metodo get de la clase Clinica Veterinaria que me permite obtener el nombre
+     * @return
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Metodo seet de la clase ClinicaVeterinaria que permite modificar el nombre
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //generados automáticamente
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Mascota> getListMascotas() {
+        return listMascotas;
+    }
+
+    public void setListMascotas(List<Mascota> listMascotas) {
+        this.listMascotas = listMascotas;
+    }
+
+    public List<Cita> getListCitas() {
+        return listCitas;
+    }
+
+    public void setListCitas(List<Cita> listCitas) {
+        this.listCitas = listCitas;
+    }
+
+    public List<Veterinario> getListVeterinarios() {
+        return listVeterinarios;
+    }
+
+    public void setListVeterinarios(List<Veterinario> listVeterinarios) {
+        this.listVeterinarios = listVeterinarios;
+    }
+
+    public List<Propietario> getListPropietarios() {
+        return listPropietarios;
+    }
+
+    public void setListPropietarios(List<Propietario> listPropietarios) {
+        this.listPropietarios = listPropietarios;
+    }
 }
